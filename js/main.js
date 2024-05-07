f859a8e0b1be4160e34106d1599bd90548bed9d9
TiagoSpada <tiago.spada.ts@gmail.com>
2024-05-08 00:56:39 +0200

    added script and readme

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
+// creo il ciclo for 
+for(let i=1; i<=100; i++){
+    // controllare per 15 è come controllare sia per 5 che per 3
+    if(i % 15 === 0 ){
+        console.log(i, 'FizzBuzz');
+    }else if(i % 3 === 0){
+        console.log(i, 'Fizz')
+    }else if (i % 5 === 0){
+        console.log(i, 'Buzz')
+    }else{
+        console.log(i);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+CONSEGNA
+Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+creo un ciclo che si ripete 100 volte con una variabile che aumenta a ogni ciclo
+-ogni volta controllo se è divisibile per 3 o 5 oppure entrambi 
+--stampo in console in base al risultato buzz, fizz, fizzbuzz o il numero
